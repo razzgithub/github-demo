19540988eec610e460f83e0624aa997d1a914692
BIKRAM <BIKRAM@LAPTOP-V2KAIUF1>
2022-03-24 15:07:21 +0530

    1st changes by manager

---
+++ src/test/java/com/crm/org/CreateOrgTest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.crm.org;
+
+/**
+ * @author BIKRAM
+ *
+ */
+public class CreateOrgTest {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Manager-Created Organisation");
+	}
+
+}

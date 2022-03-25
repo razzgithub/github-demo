0647887e4fa561500d35e4ab49f7e5d3a87841a6
BIKRAM <BIKRAM@LAPTOP-V2KAIUF1>
2022-03-25 10:11:38 +0530

    Convert master to branch sucessfully by dev1

---
+++ src/test/java/com/crm/org/BranchCheckTest.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.crm.org;
+
+/**
+ * @author BIKRAM
+ *
+ */
+public class BranchCheckTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+		
+		System.out.println("Convert master to branch sucessfully");
+		
+
+	}
+}
